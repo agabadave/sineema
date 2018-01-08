a33d36b83d67f35cdeb4a2f7408ac79a438ebc44
Davis Agaba <davis@laboremus.no>
2018-01-08 15:27:51 +0300

    Cleaning up Solution

--- VideoLibrary/VideoLibrary/VideoLibrary/App_Start/RouteConfig.cs
+++ VideoLibrary/VideoLibrary/VideoLibrary/App_Start/RouteConfig.cs
@@ -14,7 +14,7 @@ namespace VideoLibrary
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "EF", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
             
         }
