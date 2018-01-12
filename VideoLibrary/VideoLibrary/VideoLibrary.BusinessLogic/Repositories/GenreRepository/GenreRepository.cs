26be74d918cbc403c80539fc40027df9c8d463c7
Joseph Serunjogi <joseph.serunjogi@hotmail.com>
2018-01-12 13:55:38 +0300

    Genre repository implementation.
    Implementation of the genre repository.
    
    signed-off-by: Joseph Serunjogi @joeseggie
    <joseph.serunjogi@hotmail.com>

---
+++ VideoLibrary/VideoLibrary/VideoLibrary.BusinessLogic/Repositories/GenreRepository/GenreRepository.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoLibrary.BusinessEntities;
+using VideoLibrary.BusinessEntities.Models.Model;
+
+namespace VideoLibrary.BusinessLogic.Repositories.GenreRepository
+{
+    public class GenreRepository : IGenreRepository
+    {
+        private readonly LibraryContext _db;
+
+        public GenreRepository(LibraryContext context)
+        {
+            _db = context;
+        }
+
+        /// <summary>
+        /// Add genre.
+        /// </summary>
+        /// <param name="genre">Genre to add.</param>
+        /// <returns>Task result.</returns>
+        public async Task AddGenreAsync(Genre genre)
+        {
+            using (_db)
+            {
+                _db.Genres.Add(genre);
+                await _db.SaveChangesAsync();
+            }
+        }
+
+        /// <summary>
+        /// List all genres.
+        /// </summary>
+        /// <returns>List of genres.</returns>
+        public IQueryable<Genre> GetAllGenres()
+        {
+            using (_db)
+            {
+                return _db.Genres;
+            }
+        }
+
+        /// <summary>
+        /// Get genre by Id.
+        /// </summary>
+        /// <param name="genreId">Genre Id.</param>
+        /// <returns>Genre</returns>
+        public async Task<Genre> GetGenreById(Guid genreId)
+        {
+            return await GetAllGenres().SingleOrDefaultAsync(g => g.Id == genreId);
+        }
+
+        /// <summary>
+        /// Update genre.
+        /// </summary>
+        /// <param name="genre">Genre updates.</param>
+        /// <returns>Task result.</returns>
+        public async Task UpdateGenreAsync(Genre genre)
+        {
+            var genreToUpdate = await GetGenreById(genre.Id);
+
+            if (genreToUpdate == null)
+            {
+                throw new KeyNotFoundException($"Genre with Id {genre.Id} was not found.");
+            }
+
+            genreToUpdate.Title = genre.Title;
+
+            using (_db)
+            {
+                _db.Entry(genreToUpdate).State = EntityState.Modified;
+                await _db.SaveChangesAsync();
+            }
+        }
+    }
+}
