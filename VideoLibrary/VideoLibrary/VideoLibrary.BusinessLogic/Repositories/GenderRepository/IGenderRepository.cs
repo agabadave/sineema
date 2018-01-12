757c14a0d806d8cd7e9ce1941a629b742d982a80
Joseph Serunjogi <joseph.serunjogi@hotmail.com>
2018-01-13 00:58:30 +0300

    Gender repository implementation
    Implementing the gender repository.
    
    signed-off-by: Joseph Serunjogi @joeseggie
    <joseph.serunjogi@hotmail.com>

---
+++ VideoLibrary/VideoLibrary/VideoLibrary.BusinessLogic/Repositories/GenderRepository/IGenderRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoLibrary.BusinessEntities.Models.Model;
+
+namespace VideoLibrary.BusinessLogic.Repositories.GenderRepository
+{
+    public interface IGenderRepository
+    {
+        Task AddGenderAsync(Gender gender);
+
+        Task UpdateGenderAsync(Gender gender);
+
+        Task RemoveGenderAsync(Guid genderId);
+
+        Task<Gender> GetGenderById(Guid genderId);
+
+        IQueryable<Gender> GetAllGenders();
+    }
+}
