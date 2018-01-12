35f9f2d2b32d739be65339520e23e394b367ce1b
Joseph Serunjogi <joseph.serunjogi@hotmail.com>
2018-01-12 13:32:45 +0300

    Client repository implementation
    Implementing of the client repository operations.
    
    signed-off-by: Joseph Serunjogi @joeseggie
    <joseph.serunjogi@hotmail.com>

--- VideoLibrary/VideoLibrary/VideoLibrary.BusinessLogic/Repositories/ClientRepository/ClientRepository.cs
+++ VideoLibrary/VideoLibrary/VideoLibrary.BusinessLogic/Repositories/ClientRepository/ClientRepository.cs
@@ -1,26 +1,145 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using VideoLibrary.BusinessEntities;
 using VideoLibrary.BusinessEntities.Models.Model;
 
 namespace VideoLibrary.BusinessLogic.Repositories.ClientRepository
 {
-    public class ClientRepository: RepositoryBase<LibraryContext>, IClientRepository
+    public class ClientRepository : IClientRepository
     {
-        public List<Client> GetAll()
+        private readonly LibraryContext _db;
+
+        public ClientRepository(LibraryContext context)
         {
-            using (var db = new LibraryContext())
+            _db = context;
+        }
+
+        /// <summary>
+        /// Add client.
+        /// </summary>
+        /// <param name="client">Client to add.</param>
+        /// <returns>Client added.</returns>
+        public async Task<Client> AddClientAsync(Client client)
+        {
+            using (_db)
             {
-                return db.Clients.ToList();
+                var newClient = _db.Clients.Add(client);
+                await _db.SaveChangesAsync();
+
+                return newClient;
             }
         }
 
-        public Client GetClient(Guid clientId)
+        /// <summary>
+        /// List all the clients.
+        /// </summary>
+        /// <returns>List of clients.</returns>
+        public IQueryable<Client> GetAllClients()
         {
-            using (var db = new LibraryContext())
+            using (_db)
             {
-                return db.Clients.FirstOrDefault(q => q.Id == clientId);
+                return _db.Clients;
+            }
+        }
+
+        /// <summary>
+        /// Get client given the client Id.
+        /// </summary>
+        /// <param name="clientId">Id of client.</param>
+        /// <returns>Client</returns>
+        public async Task<Client> GetClientByIdAsync(Guid clientId)
+        {
+            return await GetAllClients().SingleOrDefaultAsync(client => client.Id == clientId);
+        }
+
+        /// <summary>
+        /// Get all the clients that are born on a given day in a month.
+        /// </summary>
+        /// <param name="month">Calendar month.</param>
+        /// <param name="day">Day</param>
+        /// <returns>List of clients.</returns>
+        public async Task<IEnumerable<Client>> GetClientsBornOnDay(int month, int day)
+        {
+            return await GetAllClients()
+                .Where(client => client.DateOfBirth.Value.Month == month && client.DateOfBirth.Value.Day == day).ToListAsync();
+        }
+
+        /// <summary>
+        /// Get all clients that have borrowed a movie.
+        /// </summary>
+        /// <returns>List of clients.</returns>
+        public IQueryable<Client> GetClientsBorrowedMovie()
+        {
+            using (_db)
+            {
+                return _db.BorrowedMovies.Include(b => b.Client).Select(b => b.Client);
+            }
+        }
+
+        /// <summary>
+        /// Get all the client that have borrowed a movie specified by Id.
+        /// </summary>
+        /// <param name="movieId">Movie Id.</param>
+        /// <returns>List of clients.</returns>
+        public async Task<IEnumerable<Client>> GetClientsBorrowedMovieAsync(Guid movieId)
+        {
+            using (_db)
+            {
+                return await _db.BorrowedMovies
+                    .Include(b => b.Client)
+                    .Where(b => b.MovieId == movieId)
+                    .Select(b => b.Client)
+                    .ToListAsync();
+            }
+        }
+
+        /// <summary>
+        /// Remove a client from the repository.
+        /// </summary>
+        /// <param name="clientId">Id of the client.</param>
+        /// <returns>Task result.</returns>
+        public async Task RemoveClientAsync(Guid clientId)
+        {
+            var clientToRemove = await GetAllClients().SingleOrDefaultAsync(client => client.Id == clientId);
+
+            if (clientToRemove == null)
+            {
+                throw new KeyNotFoundException($"Client with Id {clientId} was not found.");
+            }
+
+            using (_db)
+            {
+                _db.Clients.Remove(clientToRemove);
+                await _db.SaveChangesAsync();
+            }
+        }
+
+        /// <summary>
+        /// Update client.
+        /// </summary>
+        /// <param name="client">Client update details.</param>
+        /// <returns>Task result.</returns>
+        public async Task UpdateClientAsync(Client client)
+        {
+            var clientToUpdate = await GetAllClients().SingleOrDefaultAsync(c => c.Id == client.Id);
+
+            if (clientToUpdate == null)
+            {
+                throw new KeyNotFoundException($"Client with Id {client.Id} was not found.");
+            }
+
+            clientToUpdate.DateOfBirth = client.DateOfBirth;
+            clientToUpdate.FirstName = client.FirstName;
+            clientToUpdate.LastName = client.LastName;
+            clientToUpdate.GenderId = client.GenderId;
+
+            using (_db)
+            {
+                _db.Entry(clientToUpdate).State = EntityState.Modified;
+                await _db.SaveChangesAsync();
             }
         }
     }
--- VideoLibrary/VideoLibrary/VideoLibrary.BusinessLogic/Repositories/ClientRepository/IClientRepository.cs
+++ VideoLibrary/VideoLibrary/VideoLibrary.BusinessLogic/Repositories/ClientRepository/IClientRepository.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using VideoLibrary.BusinessEntities;
 using VideoLibrary.BusinessEntities.Models.Model;
 
 namespace VideoLibrary.BusinessLogic.Repositories.ClientRepository
 {
-    public interface IClientRepository : IRepositoryBase
+    public interface IClientRepository
     {
-        List<Client> GetAll();
-        Client GetClient(Guid clientId);
-        
+        IQueryable<Client> GetAllClients();
+        Task<Client> GetClientByIdAsync(Guid clientId);
+        Task<Client> AddClientAsync(Client client);
+        Task RemoveClientAsync(Guid clientId);
+        Task UpdateClientAsync(Client client);
+        IQueryable<Client> GetClientsBorrowedMovie();
+        Task<IEnumerable<Client>> GetClientsBorrowedMovieAsync(Guid movieId);
+        Task<IEnumerable<Client>> GetClientsBornOnDay(int month, int day);       
     }
 }
