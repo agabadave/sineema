917028fd663794f7c89f7ea8197b09e9945f960a
dennisMK <dmushabe1990@gmail.com>
2018-01-13 22:54:06 +0300

    DashBoard 001 Test

---
+++ VideoLibrary/VideoLibrary/VideoLibrary.BusinessEntities/Models/Model/SystemUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoLibrary.BusinessEntities.Models.Model
+{
+    public class  SystemUser : ModelBase
+    {
+       
+        [Required(ErrorMessage = "Field can't be empty")]
+        public string UserName{ get; set; }
+        [Required(ErrorMessage = "Field can't be empty")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "E-mail is not valid")]
+        public string EmailAddress { get; set; }
+        [Required(ErrorMessage = "Field can't be empty")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
